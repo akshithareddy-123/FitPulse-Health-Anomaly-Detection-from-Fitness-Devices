cc0f6f8c70e16ed40142bbeb5140bcf1f233dde1
Akshitha Reddy <aletiakshithareddy07@gmail.com>
2025-12-13 23:53:34 +0530

    app.py

--- Milestone1/app.py
+++ Milestone1/app.py
@@ -1,10 +1,10 @@
-%%writefile Milestone1/app.py
 from preprocess import preprocess_data
-
 if __name__ == "__main__":
     df = preprocess_data(
         "Milestone1/data/minuteSteps.csv",
         "Milestone1/data/dailySteps.csv",
         "Milestone1/data/sleepDay.csv"
     )
+
+    print("Processed Data:")
     print(df.head())
